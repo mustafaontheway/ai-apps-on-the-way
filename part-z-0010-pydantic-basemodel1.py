9276d10d415c9c4d7a8ddf8c72bc9442954b5b09
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-24 22:23:20 +0300

    Create part-z-0010-pydantic-basemodel1.py

---
+++ part-z-0010-pydantic-basemodel1.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+class Employee(BaseModel):
+    emp_id: int
+    name: str
+    department: str
+    salary: int
+    married: bool
+    
+    
+EMPLOYEES: List[Employee] = [
+    
+    Employee(emp_id=4296, name="Ayhan Bilir", department="Finance", salary=42000, married=True),
+    Employee(emp_id=3254, name="Hakan Kaya", department="FinTech", salary=87000, married=True),
+    Employee(emp_id=2154, name="Bengü İyi", department="HR", salary=30000, married=False),
+    Employee(emp_id=6874, name="Kul Yapıcı", department="Accounting", salary=32500, married=False),
+    Employee(emp_id=1100, name="Bumin Burada", department="Marketing", salary=66800, married=True),
+]
+
+@app.get("/employees")
+async def get_employees() -> List[Employee]:
+    return EMPLOYEES
+
+
+@app.post("/create-employee")
+async def create_employee(new_employee: Employee):
+    EMPLOYEES.append(new_employee)
+    return new_employee
